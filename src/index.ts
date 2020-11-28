ed1144d0463a039657b23abb552495d1927c2f78
lihongyao <lihy_online@163.com>
2020-11-29 00:18:09 +0800

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import useDateCountdown from './useDateCountdown';
 import useTitle from './useTitle';
 import useInterval from './useInterval';
 import useBoolean from './useBoolean';
-import useDebounce from './useDebounce'
+import useDebounce from './useDebounce';
 
 export { useDateCountdown, useTitle, useInterval, useBoolean, useDebounce };
